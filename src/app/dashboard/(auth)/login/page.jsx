365ea4a1bac11b842fd510d8d53af58570e81051
Sevinda Perera <sevindacharukaperera@gmail.com>
2023-08-20 22:18:37 +0530

    tried to make auth but didnt work. try again

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,9 +1,16 @@
+"use client";
+
 import React from "react";
+import { signIn } from "next-auth/react";
 
 import styles from "./page.module.css";
 
 const Login = () => {
-  return <div className={styles.container}>Login</div>;
+  return (
+    <div className={styles.container}>
+      <button onClick={() => signIn("google")}>Login with Google</button>
+    </div>
+  );
 };
 
 export default Login;
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,8 @@
 "use client";
 
 // import React, { useEffect, useState } from "react";
-import useSWR from 'swr'
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
 
 import styles from "./page.module.css";
 
@@ -31,9 +32,15 @@ const Dashboard = () => {
   // }, []);
 
   // console.log(data);
-  
+
+  const session = useSession();
+  console.log(session);
+
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
 
   return <div className={styles.container}>Dashboard</div>;
 };
